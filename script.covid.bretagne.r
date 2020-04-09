4a3712fcb2551ab04a73b0c3d7a82c0dc0b4305a
Maela Kloareg <42865360+MaelaKloareg@users.noreply.github.com>
2020-04-09 18:03:17 +0200

    Add files via upload

--- script.covid.bretagne.r
+++ script.covid.bretagne.r
@@ -9,66 +9,62 @@ hier=format(Sys.Date()-1,"%d/%m%/%Y")
 dateconfinement = as.Date("2020-03-17")
 
 #############################
-# cumul sur la région Bretagne
+# importation
 
 bzh = read_excel("ARS_BZH.xlsx", sheet = "covid19_ARS_Bretagne")
 
-bzh$date=as.Date(bzh$date, format="%d/%m/%Y")
-bzh$categorie=as.factor(bzh$categorie)
+bzh = read.csv2("Covid19_Bretagne.csv")
+
+bzh$Date=as.Date(bzh$Date, format="%d/%m/%Y")
 summary(bzh)
 
-ggplot(data=bzh) + aes(y=nombre, x=date, col=categorie) +geom_point() + geom_line() + theme_classic() +
+#############################
+# nombre de cas sur la région Bretagne
+
+ggplot(data=bzh) + aes(y=cas4dep, x=Date) +geom_point() + geom_line() + theme_classic() +
 	labs(y="Nombre de cas déclarés par l'ARS en région Bretagne", col="", 
-		caption=paste("données mises à jour le",aujourdhui))
-		#caption=paste("données mises à jour le",aujourdhui,"@MaelaKloareg"))
-# échelle log
-ggplot(data=bzh) + aes(y=nombre, x=date, col=categorie) +geom_point() + geom_line() + theme_classic() +
-	labs(y="Nombre de cas déclarés par l'ARS en région Bretagne (échelle logarithmique)", col="") +
-	scale_y_continuous(trans = 'log10') +
-	annotation_logticks(sides="l")
+		#caption=paste("données mises à jour le",aujourdhui))
+		caption=paste("données mises à jour le",hier,"@MaelaKloareg"))
 
 #############################
-# augmentation quotidienne de cas en Bretagne
+# nombre de morts sur la région Bretagne
 
-castbzh = dcast(bzh, date~categorie, value.var = "nombre")
-castbzh$diffcas = c(NA,diff(castbzh$cas))
-summary(castbzh)
-
-ggplot(data=castbzh) + theme_classic() +
-	aes(y=diffcas, x=date) + geom_col(fill="royalblue") +
-	labs(y="", 
-		caption=paste("données mises à jour le",aujourdhui))+
-	ggtitle("Augmentation quotidienne du nombre de cas COVID-19 déclarés par l'ARS en région Bretagne") +
-	annotate("text", x = dateconfinement , y = 40, label = "Début du confinement", angle = 90)
-
+ggplot(data=bzh) + aes(y=morts4dep, x=Date) +geom_point() + geom_line() + theme_classic() +
+	labs(y="Nombre de morts déclarés par l'ARS en région Bretagne", col="", 
+		#caption=paste("données mises à jour le",aujourdhui))
+		caption=paste("données mises à jour le",hier,"@MaelaKloareg"))
 
 #############################
 # nombre de cas par département
 
-dep = read_excel("ARS_BZH.xlsx", sheet = "covid19_ARS_cas_par_departement")
-dep$Département=as.factor(dep$Département)
+meltbzh=melt(bzh[,1:8], id.vars ="Date")
+summary(meltbzh)
 
-sort(tapply(dep$Nombre, dep$Département, max, na.rm=TRUE), decreasing = TRUE)
-DepNewLevels=names(sort(tapply(dep$Nombre, dep$Département, max, na.rm=TRUE), decreasing = TRUE))
-dep$Département=factor(dep$Département,DepNewLevels)
+levels(meltbzh$variable) =c("Morbihan" , "Ille-et-Vilaine", "Finistère",            
+"Côtes d'Armor", "Département non connu" ,"Non résidents",  "Loire-Atlantique" )
 
-summary(dep)
+depNewLevels=c("Loire-Atlantique","Ille-et-Vilaine","Morbihan", "Finistère","Côtes d'Armor","Département non connu","Non résidents")
+meltbzh$variable=factor(meltbzh$variable,depNewLevels)
 
-ggplot(data=dep) + aes(y=Nombre, x=Date, col=Département) +geom_point() + geom_line() + theme_classic() +
-	labs(y="Nombre de cas COVID-19 déclarés par l'ARS en région Bretagne", 
-		caption=paste("données mises à jour le",aujourdhui))
+ggplot(data=meltbzh) + aes(y=value, x=Date, col=variable) +geom_point() + geom_line() + theme_classic() +
+	labs(title="Nombre de cas déclarés par l'ARS Bretagne et en Loire Atlantique", col="", y="", 
+		#caption=paste("données mises à jour le",aujourdhui))
+		caption=paste("données mises à jour le",hier,"@MaelaKloareg"))+ 
+  	scale_colour_brewer(palette="Set1")
 
-# échelle log
-ggplot(data=dep) + aes(y=Nombre, x=Date, col=Département) +geom_point() + geom_line() + theme_classic() +
-	scale_y_continuous(trans = 'log10') +
-	annotation_logticks(sides="l") + 
-	labs(y="Nombre total de cas déclarés par l'ARS (échelle logarithmique)")
+ggsave("cas_BZH.png")
 
 
-# grafik e bzhg
-dep$Departamant=as.factor(dep$Département)
-levels(dep$Departamant)=c("Aodoù an Arvor", "Penn ar bed", "Il ha Gwilun", "Morbihan")
+#############################
+# augmentation quotidienne des cas en Bretagne
 
-ggplot(data=dep) + aes(y=Nombre, x=Date, col=Departamant) +geom_point() + geom_line() + theme_classic() +
-	labs(y="Niver a dud klañv gant ar COVID-19 diskleriet gant an ARS", x="Deiziad")
+bzh$diffcas = c(NA,diff(bzh$cas4dep))
+summary(bzh)
 
+ggplot(data=bzh) + theme_classic() +
+	aes(y=diffcas, x=Date) + geom_col(fill="royalblue") +
+	labs(y="", 
+		caption=paste("données mises à jour le",hier,"@MaelaKloareg"))+
+	ggtitle("Augmentation quotidienne du nombre de cas COVID-19 déclarés par l'ARS en région Bretagne") +
+	annotate("text", x = dateconfinement , y = 40, label = "Début du confinement", angle = 90)
+ggsave("diffcas_BZH.png")
